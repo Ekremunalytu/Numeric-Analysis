635871347ed0c46a6ef568723424213ff50aaa52
Ekremunalytu <ekremunalw1@gmail.com>
2023-04-10 01:16:44 +0300

    first loop

---
+++ main.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+int main() {
+
+    int in_program = 1;
+
+    
+
+    printf("Welcome to numeric analysis program!!!\n");
+    printf("Enter a designed number to make an matematical operation you wished for .\n");
+    
+
+    int flag;
+    
+    while (in_program){
+    
+    
+    printf("Please enter 0 for exit\n");
+    scanf("%d" , &flag);
+
+    
+    if(flag == 0 ){
+        in_program = 0;
+    }
+
+    };
+
+
+
+
+
+
+
+
+
+
+    
+}
