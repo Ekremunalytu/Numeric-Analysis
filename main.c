dfed86d356f8eb742490842f2d29781dfbfc3d03
Ekremunalytu <ekremunalw1@gmail.com>
2023-04-14 19:20:20 +0300

    loop completed and switch function is added

--- main.c
+++ main.c
@@ -1,29 +1,54 @@
 #include <stdio.h>
+#include <math.h>
+
+
+
+
+
 
 int main() {
 
-    int in_program = 1;
 
     
+    int operation;
+    int in_program = 1;
+    
+    
+
+    
+
+    
+    
+    while (in_program){
+
 
     printf("Welcome to numeric analysis program!!!\n");
     printf("Enter a designed number to make an matematical operation you wished for .\n");
+    printf("Enter 1 to reverse a matrix\n");
+    scanf("%d" , &operation);
     
 
     int flag;
-    
-    while (in_program){
-    
-    
-    printf("Please enter 0 for exit\n");
-    scanf("%d" , &flag);
+        
+            switch (operation)
+        {
+        case 1:
+            printf("it works\n");
 
-    
-    if(flag == 0 ){
-        in_program = 0;
-    }
+        default:
+            break;
+        }
+        
+        printf("Please enter 0 for exit\n");
+        scanf("%d" , &flag);
 
-    };
+        
+        
+        if(flag == 0 ){
+            in_program = 0;
+        }
+
+        };
 
 
 
