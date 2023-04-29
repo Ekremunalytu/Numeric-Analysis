f6dbfcc518a64b1e9df87fe2a3883846b5fa9de6
Ekremunalytu <ekremunalw1@gmail.com>
2023-04-29 21:45:26 +0300

    Polynom create and display function has been written. Base control operations for bisection method made

--- main.c
+++ main.c
@@ -1,5 +1,40 @@
 #include <stdio.h>
 #include <math.h>
+#include <stdlib.h>
+
+struct Term{
+    int Coefficient;
+    int Exponent;
+};
+struct Polynom{
+    int n;
+    struct Term *Terms;
+}; 
+
+void Create (struct Polynom *p){
+    int i;
+    printf("Enter Number of Terms.\n");
+    scanf("%d", &p -> n);
+
+    p -> Terms = (struct Term  *) malloc((p->n) * sizeof(struct Term));
+
+    printf("Enter Terms\n");
+    for (i = 0  ; i < p -> n ; i++){
+        scanf("%d%d" , &p->Terms[i].Coefficient , &p->Terms[i].Exponent);
+    }
+}
+
+void Print_Polynom (struct Polynom p){
+    int i;
+    for (i = 0 ; i < p.n ; i++){
+        printf("%d^%d" , p.Terms[i].Coefficient , p.Terms[i].Exponent);
+        if (i + 1 < p.n){
+            printf(" + ");
+        }
+    };
+    printf("\n");
+};
+
 
 
 
@@ -24,8 +59,29 @@ int main() {
 
     printf("Welcome to numeric analysis program!!!\n");
     printf("Enter a designed number to make an matematical operation you wished for .\n");
-    printf("Enter 1 to reverse a matrix\n");
+    printf("Enter 1 to perform bisection method\n");
     scanf("%d" , &operation);
+    if(operation == 1){ //switch case içerisinde scanf çalışmadığı için harici olarak aldım.
+        printf("Enter floor and ceiling for bisection method.\n");
+        int floor,ceiling;
+        printf("For floor: ");
+        scanf("%d" , &floor);
+        printf("\nFor ceiling: ");
+        scanf("%d" , &ceiling);
+        if(floor > ceiling){
+            printf("You entered wrong floor and ceiling value !!!!!\n");
+            printf("Switching values.\n");
+            int Bisection_Switch_Temp = floor;
+            floor = ceiling;
+            ceiling = Bisection_Switch_Temp;
+            printf("[%d,%d]" ,floor,ceiling);
+        }
+        if(floor == ceiling){
+            printf("You entered same values.\n");
+            printf("Aborting!!!");
+            return 0;
+        }
+    };
     
 
     int flag;
@@ -33,7 +89,15 @@ int main() {
             switch (operation)
         {
         case 1:
-            printf("it works\n");
+            struct Polynom p1;
+            printf("Enter polynom to find roots.\n");
+            printf("First enter base then enter explonent.\n");
+            Create(&p1);
+            Print_Polynom(p1);
+            
+
+          
+
 
         default:
             break;
